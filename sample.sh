ce499edc55eeffce8ce614095e5535121063b514
sushmita <sushmita771995@gmail.com>
2021-01-29 11:05:29 +0530

    first commit

--- sample.sh
+++ sample.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-printf -e "\t\t\t\e[35mfrontend\e[0m"
-printf -e "\e[34mInstalling Nginx\e[0m"
+echo  -e "\t\t\t\e[35mfrontend\e[0m"
+echo -n -e "\e[34mInstalling Nginx\e[0m"
 yum install nginx -y &>> /tmp/outfile
 if [ $? == 0 ]
 then
@@ -9,7 +9,7 @@ else
 echo -e "\t\t\t\t\t\e[32mFailure\e[0m"
 exit
 fi
-echo -e "\e[34mDownloading frontend docs\e[0m"
+echo -n -e "\e[34mDownloading frontend docs\e[0m"
 if [ $? == '0' ]
 then
 echo -e "\t\t\t\t\t\e[32mSUCCESS\e[0m"
@@ -18,7 +18,7 @@ echo -e "\t\t\t\t\t\e[32mFailure\e[0m"
 exit
 fi
 curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/f4b641c1-99db-46d1-8110-5c6c24ce2fb9/_apis/git/repositories/a781da9c-8fca-4605-8928-53c962282b74/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true" &>> /tmp/outfile
-echo -e "\e[34mSetting up frontend\e[0m"
+echo -n -e "\e[34mSetting up frontend\e[0m"
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip &>> /tmp/outfile
@@ -31,7 +31,7 @@ else
 echo -e "\t\t\t\t\t\e[32mFailure\e[0m".
 exit
 fi
-echo -e "\e[34mSetting up configuration\e[0m"
+echo -n -e "\e[34mSetting up configuration\e[0m"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 echo -e "\t\t\t\t\t\e[32mSUCCESS\e[0m"
 echo -e "\e[34mStarting frontend\e[0m"
