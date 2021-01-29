a9bc6c6f371d9dbf060303e24a6d1b92ad4cc15e
sushmita <sushmita771995@gmail.com>
2021-01-29 12:36:14 +0530

    canges

--- sample.sh
+++ sample.sh
@@ -43,7 +43,7 @@ status
 ;;
 
 mongo)
-  echo -n -e "\e[34mSetting up repo\e[0m\t\t"
+  echo -n -e "\e[34mSetting up repo\e[0m\t\t\t"
   echo '[mongodb-org-4.2]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
@@ -57,12 +57,14 @@ status
 echo -n -e "\e[34mUpdate config file\e[0m\t\t\t"
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 status
+systemctl enable mongod
+systemctl start mongod
 echo -n -e "\e[34mDownload schema\e[0m\t\t\t\t"
 curl -s -L -o /tmp/mongodb.zip "https://dev.azure.com/DevOps-Batches/ce99914a-0f7d-4c46-9ccc-e4d025115ea9/_apis/git/repositories/e9218aed-a297-4945-9ddc-94156bd81427/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true" &>> $LOG_FILE
 cd /tmp
 unzip mongodb.zip
- mongo < catalogue.js
- mongo < users.js
+mongo < catalogue.js
+mongo < users.js
  status
  ;;
 *)
