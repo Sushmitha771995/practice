b69ffaa68c64b68e8ad4ca7a7276f03d52068a8d
sushmita <sushmita771995@gmail.com>
2021-01-29 12:32:30 +0530

    canges

--- sample.sh
+++ sample.sh
@@ -55,7 +55,7 @@ echo -n -e "\e[34mInstalling mongo\e[0m\t\t\t"
 yum install -y mongodb-org &>> $LOG_FILE
 status
 echo -n -e "\e[34mUpdate config file\e[0m\t\t\t"
-sed -i 's/120.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 status
 echo -n -e "\e[34mDownload schema\e[0m\t\t\t\t"
 curl -s -L -o /tmp/mongodb.zip "https://dev.azure.com/DevOps-Batches/ce99914a-0f7d-4c46-9ccc-e4d025115ea9/_apis/git/repositories/e9218aed-a297-4945-9ddc-94156bd81427/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true" &>> $LOG_FILE
