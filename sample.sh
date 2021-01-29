4032cecebc4f1384cd934a8e9f4319722525e627
sushmita <sushmita771995@gmail.com>
2021-01-29 12:40:15 +0530

    canges

--- sample.sh
+++ sample.sh
@@ -62,7 +62,7 @@ status
 echo -n -e "\e[34mDownload schema\e[0m\t\t\t"
 curl -s -L -o /tmp/mongodb.zip "https://dev.azure.com/DevOps-Batches/ce99914a-0f7d-4c46-9ccc-e4d025115ea9/_apis/git/repositories/e9218aed-a297-4945-9ddc-94156bd81427/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true" &>> $LOG_FILE
 cd /tmp
-unzip mongodb.zip
+unzip mongodb.zip      &>> $LOG_FILE
 mongo < catalogue.js   &>> $LOG_FILE
 mongo < users.js       &>> $LOG_FILE
  status
