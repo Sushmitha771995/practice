8165f0ffbfef24a01efde05b68c4a146f482b4e0
sushmita <sushmita771995@gmail.com>
2021-01-30 14:35:16 +0530

    canges

--- sample.sh
+++ sample.sh
@@ -30,13 +30,11 @@ esac
 }
 
 app_service() {
-   echo -n -e "\e[34mInstalling node js\e[0m\t\t"
+echo -n -e "\e[34mInstalling node js\e[0m\t\t"
 yum install nodejs make gcc-c++ -y  &>> $LOG_FILE
 status
-
 echo -n -e "\e[34mDownloading catalogue dependcines\e[0m\t"
-
-curl -s -L -o /tmp/$COMPONENT.zip "$1"  &>> $LOG_FILE
+curl -s -L -o /tmp/$COMPONENT.zip $1  &>> $LOG_FILE
 cd /home/roboshop
 mkdir -p $COMPONENT
 cd $COMPONENT
@@ -104,7 +102,7 @@ mongo < users.js       &>> $LOG_FILE
 
 
 catalogue)
-app_service https://dev.azure.com/DevOps-Batches/f4b641c1-99db-46d1-8110-5c6c24ce2fb9/_apis/git/repositories/1a7bd015-d982-487f-9904-1aa01c825db4/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true
+app_service "https://dev.azure.com/DevOps-Batches/f4b641c1-99db-46d1-8110-5c6c24ce2fb9/_apis/git/repositories/1a7bd015-d982-487f-9904-1aa01c825db4/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
 ;;
 redis)
 echo -n -e "\e[34mInstalling Redis\e[0m\t\t"
